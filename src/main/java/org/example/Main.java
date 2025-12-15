ea4dc570e9c434f10f3f5e886fb5229860da3bfc
simpmiko <baibatyrmiras581@gmail.com>
2025-12-16 00:23:54 +0500

    init commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,7 @@
 package org.example;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
-        IO.println(String.format("Hello and welcome!,My name is miras im 18"));
+        IO.println(String.format("Hello and welcome!,My name is miras im 18 and student group of IT-2502"));
         }
     }
 
